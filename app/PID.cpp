252799608aee0d566693814228a0d6b2c0aa91d1
charan <pvcharankarthikeyan@gmail.com>
2019-11-25 16:54:01 -0500

    [PID.cpp]Made changes in accordance with the header file

--- app/PID.cpp
+++ app/PID.cpp
@@ -1,23 +1,76 @@
-/**
- *  @copyright (c) 2019 Satyarth Praveen, Ishan Patel
- *  @file    PID.cpp
- *  @author Part 1:  Satyarth Praveen (driver), Ishan Patel (navigator)
- *  @author Part 2:  Charan Karthikeyan (driver), Corbyn Yhap (navigator)
+/*
+ *  MIT License
  *
- *  @brief Source file for the PID class and its members.
+ *  Copyright (c) 2019 Charan Karthikeyan Parthasarathy Vasanthi
  *
+ *  Permission is hereby granted, free of charge, to any person obtaining a
+ *  copy of this software and associated documentation files (the "Software"),
+ *  to deal in the Software without restriction, including without
+ *  limitation the rights to use, copy, modify, merge, publish, distribute,
+ *  sublicense, and/or sell copies of the Software, and to permit persons to
+ *  whom the Software is furnished to do so, subject to the following
+ *  conditions:
  *
+ *  The above copyright notice and this permission notice shall be included
+ *  in all copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ *  THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ *  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ *  DEALINGS IN THE SOFTWARE.
  */
 
+/**
+ *  @file      PID.cpp
+ *  @author    Charan Karthikeyan Parthasarathy Vasanthi
+ *  @copyright MIT License.
+ *  @brief   The implementation of the PID class with the inherited
+ *  newPID class.
+ */
 
 #include <PID.hpp>
 
-double PID::compute(double targetSetpoint, double actualVelocity) {
-  double dt = 1;
-  double error = targetSetpoint-actualVelocity;
-  double propotional = Kp*error;
-  double integral = Ki*error*dt;
-  double differential = Kd*error/dt;
+/*
+ * @brief Implementation of the PID class.
+ */
+PID::PID() {
+  kp = 1;
+  kd = 1;
+  ki = 1;
+  dT = 1;
+}
 
-  return propotional + integral + differential;
+PID::PID(double Kp, double Ki, double Kd, double dt) {
+  kp = Kp;
+  ki = Ki;
+  kd = Kd;
+  dT = dt;
+}
+
+double PID::getKp() {
+  return kp;
+}
+
+double PID::getKi() {
+  return ki;
+}
+
+double PID::getKd() {
+  return kd;
+}
+
+double PID::compute(double targetVelocity, double actualVal) {
+  // Compute the error and the gains of the PID.
+  double error = targetVelocity - actualVal;
+  double error_kp = kp*error;
+  double error_ki = ki*error * dT;
+  double error_kd = kd*(error/dT);
+  double final_error = error_kp + error_ki + error_kd;
+  return final_error;
+}
+
+PID::~PID() {
 }
